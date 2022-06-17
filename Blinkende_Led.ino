9facd845c314fe42a65715274faf8f90968013a2
ed-schwarz <eduardosd2000@gmail.com>
2022-06-17 22:14:29 +0200

    5.4

--- Blinkende_Led.ino
+++ Blinkende_Led.ino
@@ -2,17 +2,18 @@ int einT = 0; //boolean fur Taster
 int einS = 0; //boolean fur Spannungsnulldurchgangs
 void setup () {
 //In i t i a l i s i e r t Pin 14 (LED1) a l s o u t p u t .
-    pinMode(14, OUTPUT);
+    pinMode(14, OUTPUT); //LED1
     pinMode(0, INPUT); //Ein
     pinMode(1, INPUT); //Aus
     pinMode(6, INPUT); //Spannungsnulldurchgangs
+    pinMode(3, OUTPUT); //Triac
 }
 // LED1 b l i n k t im Zw e i s e k u n d e n t a k t
 void loop () {
-    if(digitalRead(15) == HIGH){
+    if(digitalRead(0) == HIGH){ //Taster 1
         einT = 1;
     }
-    if(digitalRead(16) == HIGH){
+    if(digitalRead(1) == HIGH){ //Taster 2
         einT = 0;
     }
     if(digitalRead(16) == HIGH){
@@ -22,6 +23,8 @@ void loop () {
         else{
             digitalWrite(14, LOW); // LED1 aus
         }
+        delay(0.00001);
+        digitalWrite(3, HIGH); //Triac ein
     }
     
     
